976a5e443705f9fecc9e81aacd663dfbc3159bc0
AntoninaMed <ton.kraski2427@gmail.com>
2024-06-02 17:44:00 +0300

    task

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -5,6 +5,7 @@ export const getAllContacts = async () => {
   return contacts;
 };
 
+
 export const getContactsById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
