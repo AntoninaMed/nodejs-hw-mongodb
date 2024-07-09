bf46f89bfdcc700f90be24b7441580e84ec95f8f
AntoninaMed <ton.kraski2427@gmail.com>
2024-07-09 16:48:02 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,28 +1,18 @@
-import { isHttpError } from 'http-errors';
-import { MongooseError } from 'mongoose';
+import { HttpError } from 'http-errors';
 
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (isHttpError(err)) {
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
-      message: err.message,
+      message: err.name,
       data: err,
     });
     return;
   }
 
-  if (err instanceof MongooseError) {
-    res.status(500).json({
-      status: err.status,
-      message: 'Mongoose error',
-    });
-    return;
-  }
-
   res.status(500).json({
+    status: 500,
     message: 'Something went wrong',
-    error: err.message,
+    data: err.message,
   });
 };
-
-export default errorHandlerMiddleware;
