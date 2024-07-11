ee51feb7b982b021ab63ed8956b3f3de1761b3d6
AntoninaMed <ton.kraski2427@gmail.com>
2024-07-12 00:17:18 +0300

    update

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,3 +1,2 @@
 export { default as notFoundHandler } from './notFoundHandler.js';
 export { default as errorHandler} from './errorHandler.js';
-export { default as validateMongoId } from './validateMongoId.js';
